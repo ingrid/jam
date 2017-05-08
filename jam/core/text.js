658779a5db3ffe1df2143314813500b897e571f4
ingrid <hi@ingridcheung.com>
2017-05-08 14:12:04 -0400

    Pong rough draft, some basic text and stuff.

---
+++ jam/core/text.js
@@ -0,0 +1,78 @@
+import System from "./system";
+// A system for updating text
+// Not sure if this is the best idea but it keels it down
+// to only one render system.
+export default class TextSystem extends System{
+  constructor(game){
+    super(game);
+    this.required = ['buffer',
+                     'font',
+                     'size',
+                     'text',
+                     'image',
+                     'old_text'];
+  }
+
+  init(e){
+    // Shouln't have to do so much here but oh well.
+    e.color = 'white';
+    e.buffer = {};
+    e.size = {x:800, y:200};
+    e.buffer.can =  document.createElement('canvas');
+    e.buffer.can.width = e.size.x;
+    e.buffer.can.height = e.size.y;
+    e.buffer.ctx = e.buffer.can.getContext('2d');
+    e.buffer.ctx.font = e.font;
+    e.buffer.ctx.textBaseline="hanging";
+    e.buffer.ctx.fillStyle = e.color;
+    e.buffer.img = new Image;
+
+    e.buffer.ctx.clearRect(0, 0, e.buffer.can.width, e.buffer.can.height);
+    e.buffer.ctx.fillText(0, 0, e.text);
+    e.buffer.img.src = e.buffer.can.toDataURL();
+    e.image = {src : e.buffer.img,
+               width : 800,
+               height : 200
+              };
+  }
+
+  update_entity(e, game){
+    console.log(1);
+    if (e.old_text != e.text){
+      console.log(2222222222222222);
+      e.buffer.ctx.clearRect(0, 0, e.buffer.can.width, e.buffer.can.height);
+      e.buffer.ctx.fillText(e.text,0, 0);
+      e.buffer.img.src = e.buffer.can.toDataURL();
+      e.image = {src : e.buffer.img,
+                 width : 800,
+                 height : 200
+                };
+      e.old_text = e.text;
+    }
+  }
+}
+
+
+///////
+var keycode_ref = {
+  "31": "",    "32": " ",    "33": "!",    "34": "\"",    "35": "#",
+  "36": "$",    "37": "%",    "38": "&",    "39": "'",    "40": "(",
+  "41": ")",    "42": "*",    "43": "+",    "44": ",",    "45": "-",
+  "46": ".",    "47": "/",    "48": "0",    "49": "1",    "50": "2",
+  "51": "3",    "52": "4",    "53": "5",    "54": "6",    "55": "7",
+  "56": "8",    "57": "9",    "58": ":",    "59": ";",    "60": "<",
+  "61": "=",    "62": ">",    "63": "?",    "64": "@",    "65": "A",
+  "66": "B",    "67": "C",    "68": "D",    "69": "E",    "70": "F",
+  "71": "G",    "72": "H",    "73": "I",    "74": "J",    "75": "K",
+  "76": "L",    "77": "M",    "78": "N",    "79": "O",    "80": "P",
+  "81": "Q",    "82": "R",    "83": "S",    "84": "T",    "85": "U",
+  "86": "V",    "87": "W",    "88": "X",    "89": "Y",    "90": "Z",
+  "91": "[",    "92": "\\",    "93": "]",    "94": "^",    "95": "_",
+  "96": "`",    "97": "a",    "98": "b",    "99": "c",    "100": "d",
+  "101": "e",    "102": "f",    "103": "g",    "104": "h",    "105": "i",
+  "106": "j",    "107": "k",    "108": "l",    "109": "m",    "110": "n",
+  "111": "o",    "112": "p",    "113": "q",    "114": "r",    "115": "s",
+  "116": "t",    "117": "u",    "118": "v",    "119": "w",    "120": "x",
+  "121": "y",    "122": "z",    "123": "{",    "124": "|",    "125": "}",
+  "126": "~",    "127": ""
+}
